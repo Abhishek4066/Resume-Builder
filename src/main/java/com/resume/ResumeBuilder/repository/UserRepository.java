5aa01714995cd0a45a9a1120cbf26146bfdb4869
Abhishek Gawade <abhishekgawade227@gmail.com>
2024-01-01 19:26:24 +0530

    added jpa repository

---
+++ src/main/java/com/resume/ResumeBuilder/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.resume.ResumeBuilder.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.resume.ResumeBuilder.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+	Optional<User> findByUserName(String userName);
+
+}
