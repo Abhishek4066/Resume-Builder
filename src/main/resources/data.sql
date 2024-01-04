37bddab4e012d1400db3bb5e26f16507e10067ab
Abhishek Gawade <abhishekgawade227@gmail.com>
2024-01-04 16:17:03 +0530

    added other fields in edit page

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,5 +7,5 @@ insert into user_details (id, user_name, password, active, roles) values
 
 insert into user_profile (id, user_name, theme, summary, first_name, last_name, email, phone, designation) values
 (1, 'einstein', 1, 'Profile One Developed the theory of relativity, one of the two pillars of modern physics. My work is also known for its influence on the philosophy of science.', 'Albert', 'Einstein', 'einstein@gmail.com', '111-111-1111', 'Theoretical physicist'),
-(2, 'admin', 2, 'Profile 2 Widely recognised as one of the most influential scientists of all time and as a key figure in the scientific revolution', 'Isaac', 'admin', 'newton@gmail.com', '222-222-2222', 'Mathematician, physicist, astronomer, theologian, and author'),
+(2, 'admin', 1, 'Profile 2 Widely recognised as one of the most influential scientists of all time and as a key figure in the scientific revolution', 'Isaac', 'admin', 'newton@gmail.com', '222-222-2222', 'Mathematician, physicist, astronomer, theologian, and author'),
 (3, 'three', 3, 'Profile 3 Widely recognised as one of the most influential scientists of all time and as a key figure in the scientific revolution', 'ronnie', 'colman', 'colman@gmail.com', '222-123-2222', 'sd, cody, wdightn, theologian, and author');
