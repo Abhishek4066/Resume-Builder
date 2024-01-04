85a46deff73f2583d09860d5f8787b5234393d3a
Abhishek Gawade <abhishekgawade227@gmail.com>
2024-01-04 22:10:23 +0530

    added edit and delete functionality

--- src/main/java/com/resume/ResumeBuilder/controller/HomeController.java
+++ src/main/java/com/resume/ResumeBuilder/controller/HomeController.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.resume.ResumeBuilder.model.Education;
 import com.resume.ResumeBuilder.model.Experience;
@@ -86,9 +88,9 @@ public class HomeController {
 
 		return "profile";
 	}
-
+// make a post reques to add eduation so that it gets saved
 	@GetMapping("/edit")
-	public String edit(Model model, Principal principal) {
+	public String edit(Model model, Principal principal,@RequestParam(required = false)String add) {
 
 		String userName = principal.getName();
 
@@ -99,7 +101,17 @@ public class HomeController {
 		userProfileOptional.orElseThrow(() -> new RuntimeException("Not found: " + userName));
 
 		UserProfile userProfile = userProfileOptional.get();
-
+		
+		if ("experience".equals(add)) {
+            userProfile.getJobExperience().add(new Experience());
+           
+        } else if ("education".equals(add)) {
+            userProfile.getEducations().add(new Education());
+           
+        } else if ("skill".equals(add)) {
+            userProfile.getSkills().add("");
+        }
+		 
 		model.addAttribute("userProfile", userProfile);
 
 		return "profile-edit";
@@ -130,9 +142,15 @@ public class HomeController {
 	}
 
 	@GetMapping("/view/{userName}") // change userId to username
-	public String view(@PathVariable String userName, Model model) {
+	public String view(@PathVariable String userName, Model model,Principal principal) {
 		// Do something with userId, e.g., fetch user details from the database
 		// and add them to the model
+		
+		if (principal != null && principal.getName() != "") {
+            boolean currentUsersProfile = principal.getName().equals(userName);
+            model.addAttribute("currentUsersProfile", currentUsersProfile);
+        }
+        //String userName = principal.getName();
 
 		Optional<UserProfile> userProfileOptional = profileRepository.findByUserName(userName);
 
@@ -150,4 +168,21 @@ public class HomeController {
 		return "profile-templates/"+ userProfile.getTheme() + "/index";
 	}
 
+	
+	@GetMapping("/delete")
+    public String delete(Model model, Principal principal, @RequestParam String type, @RequestParam int index) {
+        String userName = principal.getName();
+        Optional<UserProfile> userProfileOptional = profileRepository.findByUserName(userName);
+        userProfileOptional.orElseThrow(() -> new RuntimeException("Not found: " + userName));
+        UserProfile userProfile = userProfileOptional.get();
+        if ("experience".equals(type)) {
+            userProfile.getJobExperience().remove(index);
+        } else if ("education".equals(type)) {
+            userProfile.getEducations().remove(index);
+        } else if ("skill".equals(type)) {
+            userProfile.getSkills().remove(index);
+        }
+        profileRepository.save(userProfile);
+        return "redirect:/edit";
+    }
 }
